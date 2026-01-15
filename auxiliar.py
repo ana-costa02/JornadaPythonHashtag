bcdfede605c61480c82fe386e80a821f4844e60e
ana-costa02 <anacostaa.20@gmail.com>
2026-01-15 19:27:31 -0300

    primeiro commit - automação em python para cadastramento de produtos em sistema virtual

---
+++ auxiliar.py
@@ -0,0 +1,9 @@
+import time
+import pyautogui
+
+time.sleep(5)
+print(pyautogui.position()) 
+
+#login - coordenadas Point(x=982, y=371)
+#senha - coordenadas logar Point(x=1001, y=549)
+#código do produto - coordenadas Point(x=970, y=253)
